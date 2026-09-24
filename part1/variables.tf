256d16576e26fba9581c6c71a8ebcee57d834a97
muskanmittal015 <79045871+muskanmittal015@users.noreply.github.com>
2026-09-24 10:16:46 +0530

    Update variables.tf

--- part1/variables.tf
+++ part1/variables.tf
@@ -3,7 +3,11 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
-
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "ecs-rds-demo"
+}
 variable "vpc_cidr" {
   description = "VPC CIDR"
   type        = string
