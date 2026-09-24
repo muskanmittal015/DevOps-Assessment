153862d1b8f6dba17dcd40761e0611dcb052a9c6
muskanmittal015 <79045871+muskanmittal015@users.noreply.github.com>
2026-09-24 10:30:47 +0530

    Update main.tf with ECS task definition

--- part1/main.tf
+++ part1/main.tf
@@ -221,3 +221,73 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${local.name}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = "nginx:latest"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = "/ecs/${local.name}"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+}
+resource "aws_ecs_service" "app" {
+  name            = "${local.name}-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+
+  desired_count = 2
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+    subnets = aws_subnet.private[*].id
+
+    security_groups = [
+      aws_security_group.ecs.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+
+  depends_on = [
+    aws_lb_listener.http
+  ]
+}
+resource "aws_db_subnet_group" "main" {
+  name = "${local.name}-db-subnet-group"
+
+  subnet_ids = aws_subnet.private[*].id
+}
+
